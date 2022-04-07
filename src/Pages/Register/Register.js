0daa012e017da3b8ff0ee976d9b4fb7811c6f239
Salemah <kadertanvir1@gmail.com>
2022-04-07 21:51:33 +0600

    regiter done

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './../Login/login.css';
 const Register = () => {
+    const [regdata, setRegData] = useState('');
+    const [regsuccess,setRegsuccess] = useState(false);
+    const handleOnChange = e => {
+        const feild = e.target.name;
+        const value = e.target.value;
+        const newData = { ...regdata };
+        newData[feild] = value;
+        setRegData(newData);
+        
+
+    }
+    const handleonSubmit = e => {
+        const data = {
+            ...regdata
+        }
+        console.log(regdata);
+        // const data = {
+        //     username: regdata.username,
+        //     name: regdata.rname,
+        //     email: regdata.email,
+        //     password: regdata.password,
+        //     confirmpassword: regdata.confirmpassword
+        // };
+         axios.post('http://localhost:8000/api/register/submit', data)
+            .then(res => {
+                if(res.status=200){
+                    setRegsuccess(true);
+                    setRegData('');
+                    console.log(res);
+                
+                }
+            })
+        e.preventDefault();
+    }
+
+
+
     return (
         <div className='login-form'>
             <div className="container login-form">
@@ -11,28 +49,31 @@ const Register = () => {
                         <div className="top-header">
                         <h4>Register</h4>
                         </div>
-                        <form >
+                        <form  onSubmit={handleonSubmit}>
                             <div class="mb-3">
                                 <input type="text" 
                                 name='name' 
                                 placeholder='Enter  Name' 
                                 class="form-control  " 
+                                onBlur={handleOnChange}
                                 id='form-input1'/>
 
                             </div>
                             <div class="mb-3">
                                 <input type="text" 
-                                name='uname' 
+                                name='username' 
                                 placeholder='Enter  username' 
                                 class="form-control  " 
+                                onBlur={handleOnChange}
                                 id='form-input1'/>
 
                             </div>
                             <div class="mb-3">
-                                <input type="email" 
+                                <input type="text" 
                                 name='email' 
                                 placeholder='Enter  Email' 
                                 class="form-control  " 
+                                onBlur={handleOnChange}
                                 id='form-input1'/>
 
                             </div>
@@ -41,13 +82,15 @@ const Register = () => {
                                 name='password'
                                 placeholder='Enter  password' 
                                  class="form-control "
+                                 onBlur={handleOnChange}
                                  id='form-input2' />
                             </div>
                             <div class="mb-3">
                                 <input type="password" 
-                                name='cpassword'
+                                name='confirmpassword'
                                 placeholder='Enter Confirm password' 
                                  class="form-control "
+                                 onBlur={handleOnChange}
                                  id='form-input2' />
                             </div>
                             <button type="submit" class="btn btn-primary login-submit-button">Register</button>
